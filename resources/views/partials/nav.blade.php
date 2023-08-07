e2634227e02636406ec22c764ee54f1ebe6fbfa9
sesai <sesai.cornejo@gmail.com>
2023-08-07 14:51:41 -0600

    diseño nav

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,3 +1,23 @@
+<style>
+    .nav-link{
+        color: #FFF;
+        text-align: center;
+        font-family: Proxima Nova;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal; 
+    }
+
+    .navbar .navbar-nav .nav-link.active{
+        color: #fff !important;
+        font-weight: 600;
+    }
+
+    .navbar .navbar-nav .nav-link:hover{
+         color: #fff !important;
+    }
+</style>
     <nav class="navbar navbar-expand-lg navbar-light sticky-top">
         <div class="container">
             <!-- <a class="navbar-brand" href="#">Prixima</a> -->
@@ -11,19 +31,19 @@
             <div class="collapse navbar-collapse mt-4" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('quienes-somos')}}"> @if(Route::current()->getName() == 'quienes-somos') <span class="dot">.</span> @endif Quienes somos</a>
+                        <a class="nav-link {{ Route::current()->getName() ==  'quienes-somos' ? 'active' : ''  }}" href="{{route('quienes-somos')}}"> @if(Route::current()->getName() == 'quienes-somos') <span class="dot">.</span> @endif Quienes somos</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('que-hacemos')}}">@if(Route::current()->getName() == 'que-hacemos') <span class="dot">.</span> @endif ¿Qué hacemos?</a>
+                        <a class="nav-link {{ Route::current()->getName() ==  'que-hacemos' ? 'active' : ''  }}" href="{{route('que-hacemos')}}">@if(Route::current()->getName() == 'que-hacemos') <span class="dot">.</span> @endif ¿Qué hacemos?</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('nuestro-aliados')}}">@if(Route::current()->getName() == 'nuestro-aliados') <span class="dot">.</span> @endif Nuestros aliados</a>
+                        <a class="nav-link {{ Route::current()->getName() ==  'nuestro-aliados' ? 'active' : ''  }}" href="{{route('nuestro-aliados')}}">@if(Route::current()->getName() == 'nuestro-aliados') <span class="dot">.</span> @endif Nuestros aliados</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('galeria')}}">@if(Route::current()->getName() == 'galeria') <span class="dot">.</span> @endif Galería</a>
+                        <a class="nav-link {{ Route::current()->getName() ==  'galeria' ? 'active' : ''  }}" href="{{route('galeria')}}">@if(Route::current()->getName() == 'galeria') <span class="dot">.</span> @endif Galería</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="{{route('quienes-somos')}}#contacto">Contacto</a>
+                        <a class="nav-link {{ Route::current()->getName() ==  'quienes-somos' ? 'active' : ''  }}" href="{{route('quienes-somos')}}#contacto">Contacto</a>
                     </li>
                 </ul>
             </div>
