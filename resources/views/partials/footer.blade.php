474c8799716e21b0ac0a1527c0f9fe1ed5842f63
sesai <sesai.cornejo@gmail.com>
2023-08-17 16:07:49 -0600

    target blank footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -28,14 +28,14 @@
                                             Síguenos en:
                                         </li>
                                         <li class="mb-2">
-                                            <a href="#"><img src="{{ URL::to('/') }}/images/icon_facebook.png" alt="" style="width:17px; height: 29px;"></a>
-                                            <a href="https://instagram.com/orgcas?igshid=MzRlODBiNWFlZA=="><img src="{{ URL::to('/') }}/images/icon_insta.png" alt="" style="width:26px; height: 26px;"></a>
-                                            <a href="https://twitter.com/orgcas?s=21&t=xBR-l_tTJjbAu9wO8OVC7A"><img src="{{ URL::to('/') }}/images/icon_twitter.png" alt="" style="width:23px; height: 20px;"></a>
+                                            <a href="#" target="_blank"><img src="{{ URL::to('/') }}/images/icon_facebook.png" alt="" style="width:17px; height: 29px;"></a>
+                                            <a href="https://instagram.com/orgcas?igshid=MzRlODBiNWFlZA==" target="_blank"><img src="{{ URL::to('/') }}/images/icon_insta.png" alt="" style="width:26px; height: 26px;"></a>
+                                            <a href="https://twitter.com/orgcas?s=21&t=xBR-l_tTJjbAu9wO8OVC7A" target="_blank"><img src="{{ URL::to('/') }}/images/icon_twitter.png" alt="" style="width:23px; height: 20px;"></a>
                                         </li>
                                         <li class="mb-2">
-                                            <a href="https://www.linkedin.com/company/orgcas/"><img src="{{ URL::to('/') }}/images/icon_in.png" alt="" style="width:22px; height: 22px;"></a>
-                                            <a href="https://youtube.com/@orgcas3736"><img src="{{ URL::to('/') }}/images/icon_youtube.png" alt="" style="width:29px; height: 18px;"></a>
-                                            <a href="https://www.tiktok.com/@orgcas?_t=8eIF9MoLc86&_r=1"><img src="{{ URL::to('/') }}/images/icon_tiktok.png" alt="" style="width:24px; height: 28px;"></a>
+                                            <a href="https://www.linkedin.com/company/orgcas/" target="_blank"><img src="{{ URL::to('/') }}/images/icon_in.png" alt="" style="width:22px; height: 22px;"></a>
+                                            <a href="https://youtube.com/@orgcas3736" target="_blank"><img src="{{ URL::to('/') }}/images/icon_youtube.png" alt="" style="width:29px; height: 18px;"></a>
+                                            <a href="https://www.tiktok.com/@orgcas?_t=8eIF9MoLc86&_r=1" target="_blank"><img src="{{ URL::to('/') }}/images/icon_tiktok.png" alt="" style="width:24px; height: 28px;"></a>
                                         </li>
                                     </ul>
                                 </div>
